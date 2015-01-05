386081f5635fe40d770940d6f24d7d6c0babf922
yck1509 <confuser.net@gmail.com>
2015-01-05 18:12:51 +0800

    Update AssemblyInfo

--- ICSharpCode.Decompiler/Properties/AssemblyInfo.template.cs
+++ ICSharpCode.Decompiler/Properties/AssemblyInfo.template.cs
@@ -25,3 +25,13 @@ using System.Runtime.InteropServices;
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly",
 	Justification = "AssemblyInformationalVersion does not need to be a parsable version")]
+
+internal static class RevisionClass {
+	public const string Major = "2";
+	public const string Minor = "1";
+	public const string Build = "0";
+	public const string Revision = "$INSERTREVISION$";
+	public const string VersionName = null;
+
+	public const string FullVersion = Major + "." + Minor + "." + Build + ".$INSERTREVISION$$INSERTBRANCHPOSTFIX$$INSERTVERSIONNAMEPOSTFIX$";
+}
